480020ff20c2be01963a4cd78c8c602cec6b2928
Anechek <anechek1@yandex.ru>
2017-11-26 17:46:44 +0300

    Исправила замечания в задании №9 Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -9,31 +9,34 @@ function getRandomIntegerValue(minValue, maxValue) {
   return Math.round(Math.random() * (maxValue - minValue) + minValue);
 }
 // Функция для заполнения массива волшебников
-function getArrayWizards(wizards) {
+function getArrayWizards() {
+  var array = [];
   for (var i = 0; i < WIZARDS_COUNT; i++) {
     var randomValue = getRandomIntegerValue(0, 7);
-    wizards.push({name: WIZARDS_NAMES[randomValue] + ' ' + WIZARDS_SURNAMES[randomValue], coatColor: WIZARDS_COATCOLORS[getRandomIntegerValue(0, 5)], eyesColor: WIZARDS_EYESCOLORS[getRandomIntegerValue(0, 4)]});
+    array.push({name: WIZARDS_NAMES[randomValue] + ' ' + WIZARDS_SURNAMES[randomValue], coatColor: WIZARDS_COATCOLORS[getRandomIntegerValue(0, 5)], eyesColor: WIZARDS_EYESCOLORS[getRandomIntegerValue(0, 4)]});
   }
+  return array;
 }
 // Функция для клонирования волшебников из шаблона
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardElement;
 };
+// Функция заполнения волшебников из массива
+function addWizardsFromArray(wizardsArray) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizardsArray.length; i++) {
+    fragment.appendChild(renderWizard(wizardsArray[i]));
+  }
+  similarListElement.appendChild(fragment);
+}
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
-var wizards = [];
-getArrayWizards(wizards);
-// Функция заполнения волшебников из массива
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-similarListElement.appendChild(fragment);
+var wizards = getArrayWizards();
+addWizardsFromArray(wizards);
 document.querySelector('.setup-similar').classList.remove('hidden');
